a2b0189fde1af97a8c3b25d8c285ef7d8d79c083
ahmed <mido44104@gmail.com>
2019-07-31 06:38:37 +0200

    Branch Get All Paged ==> GellAll  [Post]

--- Codes.Services/Interfaces/IBranchServices.cs
+++ Codes.Services/Interfaces/IBranchServices.cs
@@ -3,12 +3,15 @@ using Codes.Services.Core;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using Tenets.Common.Core;
 using Tenets.Common.ServicesCommon.Codes.Interface;
+using Tenets.Common.ServicesCommon.Codes.Parameters;
 
 namespace Codes.Services.Interfaces
 {
     public interface IBranchServices : IBaseService<Branch, IBranchDto>
     {
-
+        Task<IDataPagging> GetAllPaggedAsync(BranchSearchCriteriaParameters parameters);
     }
 }
