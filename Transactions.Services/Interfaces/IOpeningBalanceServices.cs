d33e3f178aa6ff76755b80e7b1684b40d3e08978
ahmed <mido44104@gmail.com>
2019-09-06 09:30:46 +0200

    Revert "GetAllPaged for opening Balance"
    This reverts commit d52ff05304f7d09d86e46a2ded0cd4240145b577.

--- Transactions.Services/Interfaces/IOpeningBalanceServices.cs
+++ Transactions.Services/Interfaces/IOpeningBalanceServices.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Threading.Tasks;
-using Tenets.Common.Core;
-using Tenets.Common.ServicesCommon.Identity.Base;
 using Tenets.Common.ServicesCommon.Transaction.Interface;
-using Tenets.Common.ServicesCommon.Transaction.Parameters;
 using Transactions.Entities.Entites;
 using Transactions.Services.Core;
 
@@ -13,7 +9,5 @@ namespace Transactions.Services.Interfaces
 {
     public interface IOpeningBalanceServices : IBaseService<OpeningBalance, IOpeningBalanceDto>
     {
-        Task<IDataPagging> GetAllPaggedAsync(BaseParam<OpeningBalanceFilter> filter);
-
     }
 }
--- Transactions.Services/Services/OpeningBalanceServices.cs
+++ Transactions.Services/Services/OpeningBalanceServices.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Net;
 using System.Text;
-using System.Threading.Tasks;
-using LinqKit;
 using Microsoft.AspNetCore.Http;
-using Tenets.Common.Core;
-using Tenets.Common.ServicesCommon.Identity.Base;
 using Tenets.Common.ServicesCommon.Transaction.Interface;
-using Tenets.Common.ServicesCommon.Transaction.Parameters;
 using Transactions.Entities.Entites;
 using Transactions.Services.Core;
-using Transactions.Services.Dto;
 using Transactions.Services.Interfaces;
 
 namespace Transactions.Services.Services
@@ -21,41 +13,6 @@ namespace Transactions.Services.Services
     {
         protected internal OpeningBalanceServices(IServiceBaseParameter<OpeningBalance> businessBaseParameter, IHttpContextAccessor httpContextAccessor) : base(businessBaseParameter, httpContextAccessor)
         {
-        }
-
-        public async Task<IDataPagging> GetAllPaggedAsync(BaseParam<OpeningBalanceFilter> filter)
-        {
-            try
-            {
-                int limit = filter.PageSize;
-                int offset = ((--filter.PageNumber) * filter.PageSize);
-                var query = await _unitOfWork.Repository.FindPaggedAsync(predicate: PredicateBuilderFunction(filter.Filter), skip: offset, take: limit, filter.OrderByValue);
-                var data = Mapper.Map<IEnumerable<OpeningBalanceDto>>(query.Item2);
-                return new DataPagging(++filter.PageNumber, filter.PageSize, query.Item1, ResponseResult.PostResult(data, status: HttpStatusCode.OK, message: HttpStatusCode.OK.ToString()));
-            }
-            catch (Exception e)
-            {
-                result.Message = e.InnerException != null ? e.InnerException.Message : e.Message;
-                result = new ResponseResult(null, status: HttpStatusCode.InternalServerError, exception: e, message: result.Message);
-                return new DataPagging(0, 0, 0, result);
-            }
-        }
-        static Expression<Func<OpeningBalance, bool>> PredicateBuilderFunction(OpeningBalanceFilter filter)
-        {
-            var predicate = PredicateBuilder.New<OpeningBalance>(true);
-            if (filter.OpeningBalanceFilterDate != null)
-            {
-                predicate = predicate.And(b => b.OpeningBlanceDate == filter.OpeningBalanceFilterDate);
-            }
-            if (filter.Type != null)
-            {
-                predicate = predicate.And(b => b.Type == filter.Type);
-            }
-            if (filter.TypeId != null)
-            {
-                predicate = predicate.And(b => b.TypeId == filter.TypeId);
-            }
-            return predicate;
         }
     }
 }
